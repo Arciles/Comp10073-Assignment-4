5b0f1f8ed7bd63a3b3dedeb634844c803ec0ea32
Esat IBIS <esat.taha.ibis@gmail.com>
2016-04-14 11:46:22 -0400

    createJs added and canvas added to the project

--- scripts/app.js
+++ scripts/app.js
@@ -75,5 +75,93 @@
 		window.location.href = "#page-top";
 	});
 
+	// ------------------ Banner Ad section ----------------------
+
+	//global variables
+	var screenWidth = 480;
+	var screenHeight = 60;
+
+	// reference to canvas element
+	var canvas = document.getElementById("canvas");
+	canvas.setAttribute("width", screenWidth.toString());
+	canvas.setAttribute("height", screenHeight.toString());
+
+	// create a stage container object
+	var stage = new createjs.Stage(canvas);
+
+	var helloLabel = null;
+	var helloLabelMove = 5;
+
+	var button = null;
+	var buttonMove = 5;
+
+	function init() {
+		console.log("Initialization");
+		// enable mouseover effects for all buttons
+		stage.enableMouseOver(20);
+
+		// set frame rate to 60 fps
+		createjs.Ticker.framerate = 60;
+		// listen for frame changes and call the animationLoop function
+		createjs.Ticker.addEventListener("tick", animationLoop);
+
+		// call the main function
+		main();
+	}
+	function animationLoop() {
+		/*helloLabel.rotation += 5;
+		 button.rotation += 5;
+		 helloLabel.x += helloLabelMove;
+		 button.x += buttonMove;
+		 if ((helloLabel.x >= screenWidth) || (helloLabel.x <= 0)) {
+		 helloLabelMove *= -1;
+		 buttonMove *= -1;
+		 }
+
+		 // refresh the stage object
+		 stage.update();*/
+
+
+
+	}
+
+	// this is where all the magic happens
+	function main() {
+		/*button = new createjs.Bitmap('/COMP1073%20-%20Lesson12%20-%20master/Assets/images/button.jpg');
+		 button.regX = button.getBounds().width * 0.5;
+		 button.regY = button.getBounds().height * 0.5;
+		 button.scaleX = 0.3;
+		 button.scaleY = 0.3;
+		 button.x = screenWidth * 0.5;
+		 button.y = screenHeight * 0.5;
+		 stage.addChild(button);
+
+		 helloLabel = new createjs.Text("Hello World!", "40px Consolas", "#000000");
+		 helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+		 helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+		 helloLabel.x = screenWidth * 0.5;
+		 helloLabel.y = screenHeight * 0.5;
+		 stage.addChild(helloLabel);
+
+		 button.on("click", function() {
+		 helloLabel.text = "Clicked!";
+		 helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+		 helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+		 });
+
+		 button.on('mouseover', function() {
+		 button.alpha = 0.5;
+		 });
+
+		 button.on('mouseout', function() {
+		 button.alpha = 1;
+		 });*/
+
+
+
+
+	}
+
+	window.onload = init;
 
 })();
